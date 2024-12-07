9a651417d2e6cc0c326483a24681b9f3a77aa0d5
Luong <luong@example.com>
2024-12-07 20:47:35 +0700

    vô đây đi

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Page loaded');
 });
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('nút vũ sẽ');
+    
+    button.addEventListener('click', () => {
+        alert('Button was clicked!');
+    });
+});
